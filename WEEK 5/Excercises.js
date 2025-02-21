eb0a5fb72ea8f612db89be716185346eb0b8e3e0
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-21 11:20:40 +0300

    Water availability

--- WEEK 5/Excercises.js
+++ WEEK 5/Excercises.js
@@ -21,3 +21,13 @@ if (score >= 70) {
 }
 
 //
+let waterAvailable = 300;
+
+if (waterAvailable > 500) {
+    console.log("Enough Water.");
+} else if (waterAvailable >= 200 && waterAvailable <= 500) {
+    console.log("Water is limited.");
+} else {
+    console.log("Water shortage.");
+}
+
