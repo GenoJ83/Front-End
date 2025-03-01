b0ae892d60c93bf8bcef2b422ed6045bc5380ed3
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-01 11:14:18 +0300

    `Removed unnecessary App component structure and replaced with a single button element`

--- WEEK 7/my-react-journey-w1/tic-tac-toe/src/App.js
+++ WEEK 7/my-react-journey-w1/tic-tac-toe/src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <button className="square">X</button>
+
   );
 }
 
