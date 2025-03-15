634339e329a34ee8409f3318094c8bbe231a9151
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-15 11:53:33 +0300

    `Added ProfilePicture.js component with a profile picture section`

---
+++ WEEK 8/Portfolio/my_portfolio/src/ProfilePicture.js
@@ -0,0 +1,13 @@
+// ProfilePicture.js
+import React from "react";
+
+const ProfilePicture = () => {
+  return (
+    <section>
+      <h2>Profile Picture</h2>
+      <img src="Pic 2.jpeg" alt="Profile" className="profile-picture" />
+    </section>
+  );
+};
+
+export default ProfilePicture;
