7ea01eebe1ac000f144760c544b84d90048a0b0c
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-15 11:42:25 +0300

    initial

---
+++ WEEK 8/Portfolio/my_portfolio/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Header from "./components/Header";
+import PersonalDetails from "./components/PersonalDetails";
+import ProfilePicture from "./components/ProfilePicture";
+import HobbyVideo from "./components/HobbyVideo";
+import Results from "./components/Results";
+import Skills from "./components/Skills";
+import Feedback from "./components/Feedback";
+import Contact from "./components/Contact";
+import CareerGoals from "./components/CareerGoals";
+import Footer from "./components/Footer";
+
+const Portfolio = () => {
+  const [lightMode, setLightMode] = useState(false);
+
+  return (
+    <div className={lightMode ? "light-mode" : "dark-mode"}>
+      <Header lightMode={lightMode} setLightMode={setLightMode} />
+      <div className="container">
+        <PersonalDetails />
+        <ProfilePicture />
+        <HobbyVideo />
+        <Results />
+        <Skills />
+        <Feedback />
+        <Contact />
+        <CareerGoals />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Portfolio;
