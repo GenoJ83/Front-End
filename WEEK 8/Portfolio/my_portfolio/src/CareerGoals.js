406f69942d81c359cdc924ee8b47eb1449c6c612
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-15 12:18:37 +0300

    `Added CareerGoals.js file with React component`

---
+++ WEEK 8/Portfolio/my_portfolio/src/CareerGoals.js
@@ -0,0 +1,13 @@
+// CareerGoals.js
+import React from "react";
+
+const CareerGoals = () => {
+  return (
+    <section>
+      <h2>Career Goals</h2>
+      <p>I aim to become a full-stack web developer with a focus on creating innovative and user-friendly web applications. I am passionate about learning new technologies and contributing to impactful projects.</p>
+    </section>
+  );
+};
+
+export default CareerGoals;
