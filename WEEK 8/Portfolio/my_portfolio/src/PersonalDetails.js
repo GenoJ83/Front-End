049be301cb081df55261df17c461ef0d4e50d244
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-15 12:02:19 +0300

    `Added PersonalDetails.js component with personal details information`

---
+++ WEEK 8/Portfolio/my_portfolio/src/PersonalDetails.js
@@ -0,0 +1,18 @@
+// PersonalDetails.js
+import React from "react";
+
+const PersonalDetails = () => {
+  return (
+    <section>
+      <h2>Personal Details</h2>
+      <ul>
+        <li>Full Name: Geno Owor Joshua</li>
+        <li>Student ID: B20233</li>
+        <li>Course and Year: Year 2 Computer Science</li>
+        <li>Hobbies and Interests: Gaming, Fitness, and Web Development</li>
+      </ul>
+    </section>
+  );
+};
+
+export default PersonalDetails;
