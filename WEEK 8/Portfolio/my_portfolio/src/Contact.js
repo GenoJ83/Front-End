1d263f8c46316332fa34dd99893c3a8c43341396
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-15 12:21:16 +0300

    `Added Contact.js component with contact information`

---
+++ WEEK 8/Portfolio/my_portfolio/src/Contact.js
@@ -0,0 +1,15 @@
+// Contact.js
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact Information</h2>
+      <p>Email: <a href="mailto:genojoshua83@gmail.com">genojoshua83@gmail.com</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/genojoshua" target="_blank">linkedin.com/in/genojoshua</a></p>
+      <p>GitHub: <a href="https://github.com/GenoJ83" target="_blank">github.com/GenoJ83</a></p>
+    </section>
+  );
+};
+
+export default Contact;
