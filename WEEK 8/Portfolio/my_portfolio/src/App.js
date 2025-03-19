8c35b5077c4728ad71376773a963bc5fa37403fa
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-03-19 17:03:44 +0300

    "Added import statement for styles.css and updated file header"

--- WEEK 8/Portfolio/my_portfolio/src/App.js
+++ WEEK 8/Portfolio/my_portfolio/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./styles/styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Header from "./components/Header";
 import PersonalDetails from "./components/PersonalDetails";
